b19068233679fa8a4e0a78b706f93d6f3fc4dd9d
Mel Ts <layzerar@gmail.com>
2018-06-05 13:11:30 +0800

    add print_version

--- xls2any/scripts/xls2any_.py
+++ xls2any/scripts/xls2any_.py
@@ -153,9 +153,21 @@ def get_j2exc_lineno():
     return 1
 
 
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    try:
+        from .. import __version__
+    except ImportError:
+        __version__ = '???'
+    click.echo(__version__)
+    ctx.exit()
+
+
 @click.command()
 @click.argument('template', type=click.File('rb'))
 @click.option('--verbose', is_flag=True)
+@click.option('--version', is_flag=True, callback=print_version, expose_value=False, is_eager=True)
 def main(template, verbose):
     if verbose:
         @Ctx.set_abort_handler
